a94413c0a77f11cebf5855454f446952de127d0c
Eymire <taras.gordon2@gmail.com>
2026-02-22 20:52:31 +0200

    Update fastapi to 0.131.0 and remove orjson response

--- src/main.py
+++ src/main.py
@@ -1,7 +1,6 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
-from fastapi.responses import ORJSONResponse
 
 from src.api.router import router as api_router
 from src.cache import cache_storage
@@ -20,7 +19,6 @@ async def lifespan(_: FastAPI):
 def create_app() -> FastAPI:
     app = FastAPI(
         title='fastapi-app',
-        default_response_class=ORJSONResponse,
         swagger_ui_parameters={'defaultModelsExpandDepth': -1},
         docs_url='/docs' if app_settings.environment == 'development' else None,
         redoc_url='/redoc' if app_settings.environment == 'development' else None,
